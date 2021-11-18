fed72eaffc53f89c884a4cea1232a8807739e1ae
Christopher Dent <christopherdent01@gmail.com>
2021-11-17 20:18:30 -0500

    add limit to api

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,23 +1,12 @@
 class ComicsController < ApplicationController
     require 'rest-client'
 
-
- 
-    
-
     def get_comics
-
         publicKey = '8ba20045db37b24d33e34f26c4be8257'
-        hash = '4c2e71d472bde5cbb7bc4a17eac68621'
-
-
-        url = "https://gateway.marvel.com/v1/public/comics?ts=1&apikey=#{publicKey}&hash=#{hash}"
-        comics = RestClient.get(url)
-
-
-
-        render json: comics 
+        hash = '4c2e71d472bde5cbb7bc4a17eac68621'        
+        url = "https://gateway.marvel.com/v1/public/comics?ts=1&apikey=#{publicKey}&hash=#{hash}&limit=100"
+        @comics = RestClient.get(url)
+        render json: @comics 
     end 
 
-       
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,2 +1,17 @@
 class Comic < ApplicationRecord
+    require 'rest_client'
+
+    # @url
+
+    # def self.getData
+    #     response = RestClient(@url, { :content_type => :json, "Api-Key" => "put your API key here" }
+    # end
+
+    # def self.retrieve_results(myParameter)
+    #      @url = "myApiUrl.com/stuff/?putYourParamNameHere=#{myParameter}"
+    #     JSON.parse(ModelForMyApi.getData)
+    # end
+
+
+
 end
