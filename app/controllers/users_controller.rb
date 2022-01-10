f06e8e86048abd4e1acc3c0e952002eed308e642
Christopher Dent <christopherdent01@gmail.com>
2022-01-09 22:07:52 -0500

    fix major syntax error in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,40 @@
 class UsersController < ApplicationController
 
-
     def index 
-        @users = User.all 
-            if @users 
-                render json: { users: @users }
-            else 
-                render json: { status: 500, errors: ['no users found'] }
+     @users = User.all 
+     if @users 
+      render json: { users: @users }
+     else 
+       render json: { status: 500, errors: ['no users found'] }
+     end 
     end 
-
+    
     def show 
-        @user = User.find(params[:id])
-        if @user 
-            render json: { user: @user }
-        else 
-            render json: {
-                status: 500, 
-                errors: ['user not found']
-            }
-        end 
+      @user = User.find(params[:id])
+      if @user 
+       render json: { user: @user }
+      else 
+       render json: {
+       status: 500, 
+       errors: ['user not found']
+       }
+      end 
     end 
-
+    
     def create
-        @user=User.new(user_params)
-        if @user.save 
-            login! 
-            render json: { user: @user } 
-        else 
-            render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity 
-        end
+     @user=User.new(user_params)
+      if @user.save 
+        login! 
+         render json: { user: @user } 
+      else 
+        render json: {errors: user.errors.full_messages.to_sentence}, status: :unprocessable_entity 
+      end
     end
+    
+    private
 
     def user_params
-        params.permit(:username, :password)
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end 
-end
+
+  end
